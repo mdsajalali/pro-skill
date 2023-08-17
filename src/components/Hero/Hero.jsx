47a8c724993c4bbae143ca4052170961eef1b4f3
mdsajalali <mdsajalali2020@gmail.com>
2023-08-17 10:02:07 +0600

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="hero">
+        <div className="hero-bg">
+          <div className="heroTitle">
+            <h1>Learn Something New</h1>
+            <h2>Everyday Anywhere Anytime</h2>
+            <p>
+              It is a long established fact that a reader will be distracted by
+              the there are many cats and dog tuitorial series.
+            </p>
+            <button className="btn">Start 7 Days Trail</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import logo from "../../assets/images/logo.png";
+import { GrLanguage } from "react-icons/gr";
+import { CiMenuFries } from "react-icons/ci";
+import { MdClose } from "react-icons/md";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const hanldeClick = () => setClick(!click);
+  return (
+    <div className="navbar">
+      <div className="nav-container">
+        <img src={logo} alt="logo" className="logo" />
+        <ul className={click ? "ul active" : "ul"}>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#course">Course</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#feedback">Feedback</a>
+          </li>
+          <li>
+            <a href="#">
+              <GrLanguage />
+            </a>
+          </li>
+        </ul>
+        <div className="hamburger" onClick={hanldeClick}>
+          {click ? <MdClose size={20} /> : <CiMenuFries size={20} />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
